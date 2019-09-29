066223de9fa332e5eb6a189422bee5c9c7d20465
oleg-gorodnitchi <oleggorj@hotmail.com>
2019-09-29 16:42:32 -0400

    dev & testing cycles

--- gcp/deploy_resources.sh
+++ gcp/deploy_resources.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 readonly CERT_PATH=~/Downloads/sslforfree_wildcard
+
 readonly NAMESPACES=( 'dev' 'test' 'uat' )
 
 
-kubectl create -n istio-system secret tls istio-ingressgateway-certs \
-  --key $CERT_PATH/private.key --cert $CERT_PATH/certificate.crt
+kubectl create -n istio-system secret tls istio-ingressgateway-certs --key $CERT_PATH/private.key --cert $CERT_PATH/certificate.crt
 
   # Istio Gateway and three ServiceEntry resources
   kubectl apply -f ./resources/istio-gateway.yaml
@@ -14,5 +14,3 @@ kubectl create -n istio-system secret tls istio-ingressgateway-certs \
   kubectl apply -f ./resources/other/auth-policy-dev.yaml
   kubectl apply -f ./resources/other/auth-policy-test.yaml
   kubectl apply -f ./resources/other/auth-policy-uat.yaml
-
-  
