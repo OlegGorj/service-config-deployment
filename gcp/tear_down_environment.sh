b67899a14ae834b928d9c5275995f0467f49dd4e
oleg-gorodnitchi <oleggorj@hotmail.com>
2019-09-29 16:42:12 -0400

    dev & testing cycles

--- gcp/tear_down_environment.sh
+++ gcp/tear_down_environment.sh
@@ -3,24 +3,55 @@
 
 [[ -f $1 ]] || printf "File not found! Please provide valid configuration file." || exit 1
 
-readonly PROJECT=$(jq '.gcp.project' ${1} | tr -d '"' )
-readonly CLUSTER=$(jq '.gcp.cluster' ${1} | tr -d '"' )
-readonly REGION=$(jq '.gcp.region' ${1} | tr -d '"' )
-readonly MASTER_AUTH_NETS=$(jq '.gcp.nets' ${1} | tr -d '"' )
-readonly NAMESPACES=( 'dev' 'test' 'uat' )
+JSONCONFIG=$(cat $1)
 
-gcloud config set project $PROJECT
+#
+# delete k8s cluster
+#
+function destroy_cluster() {
+  PROJECT=$1
+  CLUSTER=$2
+  REGION=$3
+  ZONE=$4
+  MASTER_AUTH_NETS=$5
+  K8S_VER=$6
+  MACHINE_TYPE=$7
 
-# Delete GKE cluster (time in foreground)
-time yes | gcloud beta container clusters delete $CLUSTER --region $REGION
+  NAMESPACES=( 'dev' 'test' 'uat' )
 
-# Confirm network resources are also deleted
-gcloud compute forwarding-rules list
-gcloud compute target-pools list
-gcloud compute firewall-rules list
+  gcloud config set project $PROJECT
 
-# In case target-pool associated with Cluster is not deleted
-yes | gcloud compute target-pools delete  \
-  $(gcloud compute target-pools list \
-    --filter="region:($REGION)" --project $PROJECT \
-  | awk 'NR==2 {print $1}')
+  # Delete GKE cluster (time in foreground)
+  time yes | gcloud beta container clusters delete $CLUSTER --zone $ZONE
+
+  # Confirm network resources are also deleted
+  gcloud compute forwarding-rules list
+  gcloud compute target-pools list
+  gcloud compute firewall-rules list
+
+  # In case target-pool associated with Cluster is not deleted
+  yes | gcloud compute forwarding-rules delete --region=$REGION  \
+    $(gcloud compute forwarding-rules list \
+      --filter="region:($REGION)" --project $PROJECT \
+    | awk 'NR==2 {print $1}')
+
+  # In case target-pool associated with Cluster is not deleted
+  yes | gcloud compute target-pools delete --region=$REGION  \
+    $(gcloud compute target-pools list \
+      --filter="region:($REGION)" --project $PROJECT \
+    | awk 'NR==2 {print $1}')
+
+}
+
+CONFIGSLIST=$(jq '.gcp[] | .cluster' <<< "$JSONCONFIG" | tr -d '"')
+for k in $CONFIGSLIST; do
+    echo "zone for cluster $k is " $(jq -r --arg kluster "$k" '.gcp[] | select(.cluster == $kluster) | .zone' <<< "$JSONCONFIG" )
+    destroy_cluster \
+        $(jq -r --arg kluster "$k" '.gcp[] | select(.cluster == $kluster) | .project' <<< "$JSONCONFIG" ) \
+        $k \
+        $(jq -r --arg kluster "$k" '.gcp[] | select(.cluster == $kluster) | .region' <<< "$JSONCONFIG" ) \
+        $(jq -r --arg kluster "$k" '.gcp[] | select(.cluster == $kluster) | .zone' <<< "$JSONCONFIG" ) \
+        $(jq -r --arg kluster "$k" '.gcp[] | select(.cluster == $kluster) | .nets' <<< "$JSONCONFIG" ) \
+        $(jq -r --arg kluster "$k" '.gcp[] | select(.cluster == $kluster) | .k8s_version' <<< "$JSONCONFIG" ) \
+        $(jq -r --arg kluster "$k" '.gcp[] | select(.cluster == $kluster) | .machine_type' <<< "$JSONCONFIG" )
+done
